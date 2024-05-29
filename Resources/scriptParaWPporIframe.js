c27124f420d961ae1d617a5ef89c3f07b5707df6
garredev <rrgarredev@gmail.com>
2024-05-29 18:02:00 +0200

    carpeta de recursos para docu y  herramientas como el script que WP debe incluir para recibir el tamaño del panel de forma dinamica

--- Resources/scriptParaWPporIframe.js
+++ Resources/scriptParaWPporIframe.js
@@ -1,3 +1,11 @@
+/*
+Script que se debe incluir en la página de WP que contendrá el <iframe> para la 
+visualizacion de este front.
+Recibe el mensaje que el front envía hacia el posible elemento padre (en este caso WP)
+cada vez que se actualiza el tamaño del panel principal.
+Así, la pagina de WP, puede definir dinamicamente el tamño del <iframe> para 
+evitar los scrolls secundarios y la navegación erratica por tener páginas superpuestas.
+*/
 <script>
   window.addEventListener('message', (event) => { 
     // Verificar que el mensaje proviene del dominio correcto
